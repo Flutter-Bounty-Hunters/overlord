69f5350d8a4b208e83dc887059e48abb3dcb354f
Matt Carroll <matthew-carroll@users.noreply.github.com>
2022-12-10 15:34:14 -0800

    Trying to make follow_the_leader.dart accessible (#1)

--- lib/follow_the_leader.dart
+++ lib/follow_the_leader.dart
@@ -1,3 +1,5 @@
+library overlord;
+
 // Exports tools to integrate Overlord popovers with the follow_the_leader package,
 // which makes it easy to position various popovers near their associated content.
 export 'src/cupertino/cupertino_popover_aligners.dart';
