0e47d16862d9f99803907e0a2db3c9249014780f
Matt Carroll <matthew-carroll@users.noreply.github.com>
2022-12-12 22:10:00 -0800

    Removed unnecessary builder class

--- lib/src/menus/menu_with_pointer_follower.dart
+++ lib/src/menus/menu_with_pointer_follower.dart
@@ -2,25 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:follow_the_leader/follow_the_leader.dart';
 import 'package:overlord/src/menus/menu_with_pointer.dart';
 
-class MenuWithPointerBuilder extends StatefulWidget {
-  const MenuWithPointerBuilder({
-    Key? key,
-    required this.builder,
-  }) : super(key: key);
-
-  final void Function(BuildContext, Offset focalPoint) builder;
-
-  @override
-  State<MenuWithPointerBuilder> createState() => _MenuWithPointerBuilderState();
-}
-
-class _MenuWithPointerBuilderState extends State<MenuWithPointerBuilder> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
 /// A [MenuFocalPoint] that's calculated on demand, based on the bounding
 /// rectangle of a [Leader].
 class LeaderMenuFocalPoint implements MenuFocalPoint {
